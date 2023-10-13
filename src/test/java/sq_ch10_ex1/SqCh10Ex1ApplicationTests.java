9156d60c681be6d63b1f6e5b90973ee1e69c5fb3
osparking <jbpark03@gmail.com>
2023-10-14 06:59:04 +0900

    단지 롬복만 추가한 봄 부트 프로젝트

---
+++ src/test/java/sq_ch10_ex1/SqCh10Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sq_ch10_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh10Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
