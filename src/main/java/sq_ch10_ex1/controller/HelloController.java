5d7b7f4b0c48884ab42b6c87f24c58261f8dfb56
osparking <jbpark03@gmail.com>
2023-10-14 16:26:27 +0900

    코드 절약 - restController 사용으로

--- src/main/java/sq_ch10_ex1/controller/HelloController.java
+++ src/main/java/sq_ch10_ex1/controller/HelloController.java
@@ -1,15 +1,18 @@
 package sq_ch10_ex1.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 	@GetMapping("/hello")
-	@ResponseBody
 	public String hello() {
 		return "Hello";
 	}
-	
+
+	@GetMapping("/ciao")
+	public String ciao() {
+		return "Ciao";
+	}
+
 }
